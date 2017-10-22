4207a8def1f80b177b4932bc92127c206e2c7b4a
Jungmin Ji <fdev.jm@gmail.com>
2017-10-22 21:38:18 +0900

    checking vue auths..api

--- vue-login-api/src/App.vue
+++ vue-login-api/src/App.vue
@@ -1,40 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
-    <g-signin-button :params="googleSignInParams" @success="onSignInSuccess" @error="onSignInError">Sign In with Google</g-signin-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data: () => ({
-    googleSignInParams: {
-      client_id: '1020042898377-0d2bvmct8f5iv58kv7ei74asct8g3fbq.apps.googleusercontent.com'
-    }
-  }),
-  methods: {
-    onSignInSuccess (googleUser) {
-      const profile = googleUser.getBasicProfile()
-      console.log(profile)
-    },
-    onSignInError (error) {
-      console.log(error)
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 .g-signin-button {
   /* This is where you control how the button looks. Be creative! */
   display: inline-block;
--- vue-login-api/src/main.js
+++ vue-login-api/src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueAuthenticate from 'vue-authenticate'
 import App from './App'
 import router from './router'
-import GoogleSignInBtn from 'vue-google-signin-button'
 
-Vue.use(GoogleSignInBtn)
+Vue.use(VueResource)
+Vue.use(VueAuthenticate, {
+  baseUrl: 'http://localhost:8080'
+})
 
 Vue.config.productionTip = false
 
